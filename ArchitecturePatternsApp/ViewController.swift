e3a93d853246b18a5b713a01cdfb5cff767ec3fb
Oleg <oleg.zuev.2012@gmail.com>
2025-08-25 13:31:40 +0300

    Initial Commit

---
+++ ArchitecturePatternsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArchitecturePatternsApp
+//
+//  Created by Олег Зуев on 25.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

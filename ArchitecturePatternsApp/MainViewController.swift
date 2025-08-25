4374c04e64308d55f5355260d51e17f1f25097e8
Oleg <oleg.zuev.2012@gmail.com>
2025-08-25 21:14:23 +0300

    Done with first screen

---
+++ ArchitecturePatternsApp/MainViewController.swift
@@ -0,0 +1,106 @@
+//
+//  MainViewController.swift
+//  ArchitecturePatternsApp
+//
+//  Created by Олег Зуев on 25.08.2025.
+//
+
+import UIKit
+import SnapKit
+
+final class MainViewController: UIViewController {
+    
+    var tableView: UITableView!
+    
+    var objects = [
+        Pattern(), Pattern(), Pattern()
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        createTable()
+        setupConstraints()
+    }
+
+    private func setupUI() {
+        self.navigationItem.title = "Паттерны проектирования"
+        self.view.backgroundColor = UIColor.systemBackground
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.bullet"))
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(systemItem: .add)
+    }
+    
+    private func createTable() {
+        self.tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(PatternTableViewCell.self, forCellReuseIdentifier: PatternTableViewCell.identifier)
+        tableView.separatorStyle = .none
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+
+}
+
+
+// MARK: - UITableViewDataSource
+
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        objects.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "HeaderOne"
+        case 1:
+            return "HeaderTwo"
+        default:
+            return "Opps"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PatternTableViewCell.identifier, for: indexPath) as? PatternTableViewCell else { return UITableViewCell() }
+        
+        let object = objects[indexPath.row]
+        
+        cell.nameLabel.text = object.name
+        cell.arrowLabel.text = "˃"
+        cell.descriptionLabel.text = object.description
+        cell.patternImage.image = UIImage(named: object.image)
+        cell.viewCounterLabel.text = "Посмотрено раз: \(object.viewCounter)"
+        
+        cell.favoriteImage.image = {
+            object.isFavorite == false ? UIImage(named: "notLike") : UIImage(named: "like")
+        }()
+        
+        
+        cell.contentView.layer.cornerRadius = 25
+        cell.contentView.clipsToBounds = true
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension MainViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
